ed294eb88ee3237b206540ff6a4348c74df6d214
abbylloyd03 <abbylloyd03@gmail.com>
2022-01-22 10:42:04 -0600

    Update Module 3

---
+++ .ipynb_checkpoints/built-in-min-max-checkpoint.py
@@ -0,0 +1,16 @@
+def max_min(list):
+    """Print maximum and minimum values from a list"""
+    max_value = -1000000
+    min_value = 1000000
+    for number in list:
+        if number > max_value:
+            max_value = number
+        if number < min_value:
+            min_value = number
+    return print(list, "Maximum:", max_value, "Minimum:", min_value)
+
+list1 = [12, 27, 36]
+list2 = [12.3, 45.6, 9.7]
+         
+max_min(list1)
+max_min(list2)
